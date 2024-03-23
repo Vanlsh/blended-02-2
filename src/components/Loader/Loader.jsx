91f393687e4124f055f79911251bb951583b8eb6
Olena-Ishchenko <elena.dubovalova@gmail.com>
2024-03-23 16:11:18 +0200

    task1 finish

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,3 +1,9 @@
+import { ClipLoader } from 'react-spinners';
+import style from './Loader.module.css';
 export const Loader = () => {
-  return <div>Loader</div>;
+  return (
+    <div className={style.backdrop}>
+      <ClipLoader color="#36d7b7" />
+    </div>
+  );
 };
