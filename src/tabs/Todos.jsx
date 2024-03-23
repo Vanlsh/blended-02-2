744c240861964de98dea40a26bc4610bf24b4f00
ArtemBlyshchyk <artem.bl31@gmail.com>
2024-03-23 16:57:54 +0200

    Todos

--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -1,5 +1,27 @@
-import { Text } from 'components';
+import { Text, Form, TodoList } from 'components';
+import { nanoid } from 'nanoid';
+import { useState } from 'react';
 
 export const Todos = () => {
-  return <Text textAlign="center">There are no any todos ...</Text>;
+  const [todos, setTodos] = useState([]);
+
+  const onSubmit = text => {
+    const todo = {
+      text,
+      id: nanoid(),
+    };
+    setTodos(prevTodos => [...prevTodos, todo]);
+  };
+
+  const onDelete = id => {
+    setTodos(prevTodos => prevTodos.filter(todo => id !== todo.id));
+  };
+
+  return (
+    <>
+      <Form onSubmit={onSubmit} />
+      <Text textAlign="center">There are no any todos ...</Text>
+      <TodoList todos={todos} onDelete={onDelete} />
+    </>
+  );
 };
